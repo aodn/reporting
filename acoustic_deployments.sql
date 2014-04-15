ad11014fa90aeb0f698e1dec43fa4129b20fe032
Xavier Hoenner <xavier.hoenner@utas.edu.au>
2014-04-15 15:33:39 +1000

    Modified to point at report_test schema

--- acoustic_deployments.sql
+++ acoustic_deployments.sql
@@ -1,4 +1,4 @@
---
+﻿--
 -- PostgreSQL database dump
 --
 
@@ -8,7 +8,7 @@ SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
 
-SET search_path = reporting, pg_catalog;
+SET search_path = report_test, pg_catalog;
 
 SET default_tablespace = '';
 
