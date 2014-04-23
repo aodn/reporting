610653a3113c3f5ff9e63dd9e315a58a6206654d
Xavier Hoenner <xavier.hoenner@utas.edu.au>
2014-04-23 12:49:27 +1000

    Update reporting views by copying in the content of argo_views.sql

--- reporting_views.sql
+++ reporting_views.sql
@@ -423,23 +423,59 @@ CREATE or replace VIEW anmn_nrs_realtime_data_summary_view AS
 grant all on table anmn_nrs_realtime_data_summary_view to public;
 
 
-
+-------------------------------
+-- VIEW FOR Argo; Now using what's in the argo schema so don't need the dw_argo schema anymore.
+-------------------------------
 -- has data
 -- no change 
 -- NOTICE:  geometry_gist_joinsel called with incorrect join type
 
 CREATE or replace VIEW argo_all_deployments_view AS
-    SELECT argo_float.data_centre AS organisation, CASE WHEN (argo_float.oxygen_sensor = false) THEN 'No oxygen sensor'::text ELSE 'Oxygen sensor'::text END AS oxygen_sensor, argo_float.platform_number AS platform_code, round((argo_float.min_lat)::numeric, 1) AS min_lat, round((argo_float.max_lat)::numeric, 1) AS max_lat, round((argo_float.min_long)::numeric, 1) AS min_lon, round((argo_float.max_long)::numeric, 1) AS max_lon, COALESCE(((round((argo_float.min_lat)::numeric, 1) || '/'::text) || round((argo_float.max_lat)::numeric, 1))) AS lat_range, COALESCE(((round((argo_float.min_long)::numeric, 1) || '/'::text) || round((argo_float.max_long)::numeric, 1))) AS lon_range, date(argo_float.start_date) AS start_date, date(argo_float.last_measure_date) AS end_date, round((((date_part('day'::text, (argo_float.last_measure_date - argo_float.start_date)))::integer)::numeric / 365.242), 1) AS coverage_duration, argo_float.pi_name, CASE WHEN (date_part('day'::text, (argo_float.last_measure_date - argo_float.start_date)) IS NULL) THEN 'Missing dates'::text WHEN (argo_float.uuid IS NULL) THEN 'No metadata'::text WHEN (argo_float.data_centre IS NULL) THEN 'No organisation'::text WHEN (argo_float.pi_name IS NULL) THEN 'No principal investigator'::text ELSE NULL::text END AS missing_info FROM argo.argo_float ORDER BY argo_float.data_centre, CASE WHEN (argo_float.oxygen_sensor = false) THEN 'No oxygen sensor'::text ELSE 'Oxygen sensor'::text END, argo_float.platform_number;
+    SELECT 
+    m.data_centre AS organisation, 
+    CASE WHEN m.oxygen_sensor = false THEN 'No oxygen sensor' 
+    ELSE 'Oxygen sensor' END AS oxygen_sensor, 
+    m.platform_number AS platform_code, 
+    round((m.min_lat)::numeric, 1) AS min_lat, 
+    round((m.max_lat)::numeric, 1) AS max_lat, 
+    round((m.min_long)::numeric, 1) AS min_lon, 
+    round((m.max_long)::numeric, 1) AS max_lon, 
+    COALESCE(round((m.min_lat)::numeric, 1) || '/' || round((m.max_lat)::numeric, 1)) AS lat_range, 
+    COALESCE(round((m.min_long)::numeric, 1) || '/' || round((m.max_long)::numeric, 1)) AS lon_range, 
+    date(m.start_date) AS start_date, 
+    date(m.last_measure_date) AS end_date, 
+    round((((date_part('day', (m.last_measure_date - m.start_date)))::integer)::numeric / 365.242), 1) AS coverage_duration, 
+    m.pi_name, 
+    CASE WHEN date_part('day', (m.last_measure_date - m.start_date)) IS NULL THEN 'Missing dates' 
+    WHEN m.uuid IS NULL THEN 'No metadata' 
+    WHEN m.data_centre IS NULL THEN 'No organisation' 
+    WHEN m.pi_name IS NULL THEN 'No principal investigator'::text 
+    ELSE NULL END AS missing_info 
+    FROM argo.argo_float m
+    ORDER BY organisation, oxygen_sensor, platform_code;
 
 grant all on table argo_all_deployments_view to public;
 
-
-
--- has data
--- no change 
-
 CREATE or replace VIEW argo_data_summary_view AS
-    SELECT argo_all_deployments_view.organisation, count(DISTINCT argo_all_deployments_view.platform_code) AS no_platforms, count(CASE WHEN (date_part('day'::text, (now() - (argo_all_deployments_view.end_date)::timestamp with time zone)) < (31)::double precision) THEN 1 ELSE NULL::integer END) AS no_active_floats, count(CASE WHEN (argo_all_deployments_view.oxygen_sensor = 'Oxygen sensor'::text) THEN 1 ELSE NULL::integer END) AS no_oxygen_platforms, count(CASE WHEN ((date_part('day'::text, (now() - (argo_all_deployments_view.end_date)::timestamp with time zone)) < (31)::double precision) AND (argo_all_deployments_view.oxygen_sensor = 'Oxygen sensor'::text)) THEN 1 ELSE NULL::integer END) AS no_active_oxygen_platforms, count(CASE WHEN (argo_all_deployments_view.missing_info IS NOT NULL) THEN 1 ELSE NULL::integer END) AS no_deployments_with_missing_info, min(argo_all_deployments_view.min_lat) AS min_lat, max(argo_all_deployments_view.max_lat) AS max_lat, min(argo_all_deployments_view.min_lon) AS min_lon, max(argo_all_deployments_view.max_lon) AS max_lon, COALESCE(((min(argo_all_deployments_view.min_lat) || '/'::text) || max(argo_all_deployments_view.max_lat))) AS lat_range, COALESCE(((min(argo_all_deployments_view.min_lon) || '/'::text) || max(argo_all_deployments_view.max_lon))) AS lon_range, min(argo_all_deployments_view.start_date) AS earliest_date, max(argo_all_deployments_view.end_date) AS latest_date, round(avg(argo_all_deployments_view.coverage_duration), 1) AS mean_coverage_duration FROM argo_all_deployments_view GROUP BY argo_all_deployments_view.organisation ORDER BY argo_all_deployments_view.organisation;
+    SELECT 
+    v.organisation, 
+    count(DISTINCT v.platform_code) AS no_platforms, 
+    count(CASE WHEN date_part('day', (now() - (v.end_date)::timestamp with time zone)) < 31 THEN 1 ELSE NULL::integer END) AS no_active_floats, 
+    count(CASE WHEN v.oxygen_sensor = 'Oxygen sensor' THEN 1 ELSE NULL::integer END) AS no_oxygen_platforms, 
+    count(CASE WHEN date_part('day', (now() - (v.end_date)::timestamp with time zone)) < 31 AND v.oxygen_sensor = 'Oxygen sensor' THEN 1 ELSE NULL::integer END) AS no_active_oxygen_platforms, 
+    count(CASE WHEN v.missing_info IS NOT NULL THEN 1 ELSE NULL::integer END) AS no_deployments_with_missing_info, 
+    min(v.min_lat) AS min_lat, 
+    max(v.max_lat) AS max_lat, 
+    min(v.min_lon) AS min_lon, 
+    max(v.max_lon) AS max_lon, 
+    COALESCE(min(v.min_lat) || '/' || max(v.max_lat)) AS lat_range, 
+    COALESCE(min(v.min_lon) || '/' || max(v.max_lon)) AS lon_range, 
+    min(v.start_date) AS earliest_date, 
+    max(v.end_date) AS latest_date, 
+    round(avg(v.coverage_duration), 1) AS mean_coverage_duration 
+    FROM argo_all_deployments_view v
+    GROUP BY v.organisation 
+    ORDER BY organisation;
 
 grant all on table argo_data_summary_view to public;
 
