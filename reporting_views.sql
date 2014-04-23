5f4962a6b3696b6d281139c8baff880434b60c04
Xavier Hoenner <xavier.hoenner@utas.edu.au>
2014-04-23 11:00:52 +1000

    Update reporting views by copying in the content of abos_views.sql

--- reporting_views.sql
+++ reporting_views.sql
@@ -33,7 +33,7 @@ SET search_path = report_test, pg_catalog, public;
 
 
 -------------------------------
--- VIEWS FOR AATAMS_SATTAG_NRT; reporting views for AATAMS_SATTAG_DM do not exist yet
+-- VIEWS FOR AATAMS_SATTAG_NRT; reporting views for AATAMS_SATTAG_DM do not exist yet; Can delete the aatams_sattag manual tables in the report schema.
 -------------------------------
 -- has data
 -- :'<,'>s/aatams_sattag\./legacy_aatams_sattag./g
@@ -106,21 +106,86 @@ WITH table_a AS (
 
     grant all on table aatams_sattag_data_summary_view to public;
 
+
+-------------------------------
+-- VIEWS FOR ABOS; Now using what's in the abos schema so don't need the dw_abos schema anymore.
+-------------------------------
 -- has data
 -- :'<,'>s/abos\./dw_abos./g
 
 CREATE or replace VIEW abos_asfssots_all_deployments_view AS
-    WITH virtual_table AS (SELECT "substring"(abos_file.url, 'IMOS/ABOS/([A-Z]+)/'::text) AS sub_facility, CASE WHEN (abos_file.platform_code = 'PULSE'::text) THEN 'Pulse'::text ELSE abos_file.platform_code END AS platform_code, CASE WHEN (abos_file.deployment_code IS NULL) THEN COALESCE(((((abos_file.platform_code || '-'::text) || CASE WHEN (abos_file.deployment_number IS NULL) THEN ''::text ELSE abos_file.deployment_number END) || '-'::text) || btrim(to_char(abos_file.time_coverage_start, 'YYYY'::text)))) ELSE abos_file.deployment_code END AS deployment_code, "substring"(abos_file.url, '[^/]+nc'::text) AS file_name, ("substring"(abos_file.url, 'FV0([12]+)'::text))::integer AS file_version, CASE WHEN ("substring"(abos_file.url, '(Surface_waves|Surface_properties|Surface_fluxes|Sub-surface_temperature_pressure_conductivity|Pulse|Sub-surface_currents)'::text) = 'Pulse'::text) THEN 'Biogeochemistry'::text ELSE "substring"(abos_file.url, '(Surface_waves|Surface_properties|Surface_fluxes|Sub-surface_temperature_pressure_conductivity|Pulse|Sub-surface_currents)'::text) END AS data_category, COALESCE("substring"(abos_file.url, 'Real-time'::text), 'Delayed-mode'::text) AS data_type, COALESCE("substring"(abos_file.url, '[0-9]{4}_daily'::text), 'Whole deployment'::text) AS year_frequency, timezone('UTC'::text, abos_file.time_coverage_start) AS coverage_start, timezone('UTC'::text, abos_file.time_coverage_end) AS coverage_end, round(((date_part('day'::text, (abos_file.time_coverage_end - abos_file.time_coverage_start)) + (date_part('hours'::text, (abos_file.time_coverage_end - abos_file.time_coverage_start)) / (24)::double precision)))::numeric, 1) AS coverage_duration, (date_part('day'::text, (abos_file.last_modified - abos_file.date_created)))::integer AS days_to_process_and_upload, (date_part('day'::text, (abos_file.last_indexed - abos_file.last_modified)))::integer AS days_to_make_public, abos_file.deployment_number, abos_file.author, abos_file.principal_investigator FROM dw_abos.abos_file WHERE (abos_file.status IS DISTINCT FROM 'DELETED'::text) ORDER BY "substring"(abos_file.url, 'IMOS/ABOS/([A-Z]+)/'::text), CASE WHEN (abos_file.platform_code = 'PULSE'::text) THEN 'Pulse'::text ELSE abos_file.platform_code END, CASE WHEN ("substring"(abos_file.url, '(Surface_waves|Surface_properties|Surface_fluxes|Sub-surface_temperature_pressure_conductivity|Pulse|Sub-surface_currents)'::text) = 'Pulse'::text) THEN 'Biogeochemistry'::text ELSE "substring"(abos_file.url, '(Surface_waves|Surface_properties|Surface_fluxes|Sub-surface_temperature_pressure_conductivity|Pulse|Sub-surface_currents)'::text) END) SELECT CASE WHEN (virtual_table.year_frequency = 'Whole deployment'::text) THEN 'Aggregated files'::text ELSE 'Daily files'::text END AS file_type, COALESCE(((((virtual_table.sub_facility || '-'::text) || virtual_table.platform_code) || ' - '::text) || virtual_table.data_type)) AS headers, virtual_table.data_type, virtual_table.data_category, virtual_table.deployment_code, sum(((virtual_table.file_version = 1))::integer) AS no_fv1, sum(((virtual_table.file_version = 2))::integer) AS no_fv2, date(min(virtual_table.coverage_start)) AS coverage_start, date(max(virtual_table.coverage_end)) AS coverage_end, min(virtual_table.coverage_start) AS time_coverage_start, max(virtual_table.coverage_end) AS time_coverage_end, CASE WHEN ((virtual_table.data_type = 'Delayed-mode'::text) AND (virtual_table.year_frequency = 'Whole deployment'::text)) THEN max(virtual_table.coverage_duration) ELSE ((date(max(virtual_table.coverage_end)) - date(min(virtual_table.coverage_start))))::numeric END AS coverage_duration, round(avg(virtual_table.days_to_process_and_upload), 1) AS mean_days_to_process_and_upload, round(avg(virtual_table.days_to_make_public), 1) AS mean_days_to_make_public, virtual_table.deployment_number, virtual_table.author, virtual_table.principal_investigator, virtual_table.platform_code, virtual_table.sub_facility FROM virtual_table GROUP BY COALESCE(((((virtual_table.sub_facility || '-'::text) || virtual_table.platform_code) || ' - '::text) || virtual_table.data_type)), virtual_table.deployment_code, virtual_table.data_category, virtual_table.data_type, virtual_table.year_frequency, virtual_table.deployment_number, virtual_table.author, virtual_table.principal_investigator, virtual_table.platform_code, virtual_table.sub_facility ORDER BY CASE WHEN (virtual_table.year_frequency = 'Whole deployment'::text) THEN 'Aggregated files'::text ELSE 'Daily files'::text END, COALESCE(((((virtual_table.sub_facility || '-'::text) || virtual_table.platform_code) || ' - '::text) || virtual_table.data_type)), virtual_table.data_type, virtual_table.data_category, virtual_table.deployment_code;
-
+    WITH table_a AS (
+    SELECT 
+    "substring"(url, 'IMOS/ABOS/([A-Z]+)/') AS sub_facility, 
+    CASE WHEN platform_code = 'PULSE' THEN 'Pulse' 
+    ELSE platform_code END AS platform_code, 
+    CASE WHEN deployment_code IS NULL THEN COALESCE(platform_code || '-' || CASE WHEN (deployment_number IS NULL) THEN '' 
+    ELSE deployment_number END) || '-' || btrim(to_char(time_coverage_start, 'YYYY')) ELSE deployment_code END AS deployment_code,
+    "substring"(url, '[^/]+nc') AS file_name,
+    ("substring"(url, 'FV0([12]+)'))::integer AS file_version,
+    CASE WHEN "substring"(url, '(Surface_waves|Surface_properties|Surface_fluxes|Sub-surface_temperature_pressure_conductivity|Pulse|Sub-surface_currents)') = 'Pulse' THEN 'Biogeochemistry' 
+    ELSE "substring"(url, '(Surface_waves|Surface_properties|Surface_fluxes|Sub-surface_temperature_pressure_conductivity|Pulse|Sub-surface_currents)') END AS data_category,
+    COALESCE("substring"(url, 'Real-time'), 'Delayed-mode') AS data_type, 
+    COALESCE("substring"(url, '[0-9]{4}_daily'), 'Whole deployment') AS year_frequency, 
+    timezone('UTC'::text, time_coverage_start) AS coverage_start, 
+    timezone('UTC'::text, time_coverage_end) AS coverage_end, 
+    round(((date_part('day', (time_coverage_end - time_coverage_start)) + (date_part('hours'::text, (time_coverage_end - time_coverage_start)) / (24)::double precision)))::numeric, 1) AS coverage_duration, 
+    (date_part('day', (last_modified - date_created)))::integer AS days_to_process_and_upload, 
+    (date_part('day', (last_indexed - last_modified)))::integer AS days_to_make_public, 
+    deployment_number, author, principal_investigator 
+    FROM abos.abos_file_vw
+    WHERE status IS DISTINCT FROM 'DELETED'
+    ORDER BY sub_facility, platform_code, data_category)
+SELECT 
+CASE WHEN a.year_frequency = 'Whole deployment' THEN 'Aggregated files' 
+    ELSE 'Daily files' END AS file_type, 
+COALESCE(a.sub_facility || '-' || a.platform_code || ' - ' || a.data_type) AS headers, 
+a.data_type, 
+a.data_category, 
+a.deployment_code, 
+sum(((a.file_version = 1))::integer) AS no_fv1, 
+sum(((a.file_version = 2))::integer) AS no_fv2, 
+date(min(a.coverage_start)) AS coverage_start, 
+date(max(a.coverage_end)) AS coverage_end, 
+min(a.coverage_start) AS time_coverage_start, 
+max(a.coverage_end) AS time_coverage_end, 
+CASE WHEN a.data_type = 'Delayed-mode' AND a.year_frequency = 'Whole deployment' THEN max(a.coverage_duration) 
+    ELSE (date(max(a.coverage_end)) - date(min(a.coverage_start)))::numeric END AS coverage_duration, 
+round(avg(a.days_to_process_and_upload), 1) AS mean_days_to_process_and_upload, 
+round(avg(a.days_to_make_public), 1) AS mean_days_to_make_public, 
+a.deployment_number, a.author, 
+a.principal_investigator, 
+a.platform_code, 
+a.sub_facility 
+FROM table_a a
+GROUP BY headers, a.deployment_code, a.data_category, a.data_type, a.year_frequency, a.deployment_number, a.author, a.principal_investigator, a.platform_code, a.sub_facility 
+ORDER BY file_type, headers, a.data_type, a.data_category, a.deployment_code;
 
 grant all on table abos_asfssots_all_deployments_view to public;
 
 
--- has data
--- no changes
-
 CREATE or replace VIEW abos_asfssots_data_summary_view AS
-    SELECT abos_asfssots_all_deployments_view.file_type, abos_asfssots_all_deployments_view.headers, abos_asfssots_all_deployments_view.data_type, abos_asfssots_all_deployments_view.data_category, count(DISTINCT abos_asfssots_all_deployments_view.deployment_code) AS no_deployments, sum(abos_asfssots_all_deployments_view.no_fv1) AS no_fv1, sum(abos_asfssots_all_deployments_view.no_fv2) AS no_fv2, min(abos_asfssots_all_deployments_view.coverage_start) AS coverage_start, max(abos_asfssots_all_deployments_view.coverage_end) AS coverage_end, ceil(((date_part('day'::text, (max(abos_asfssots_all_deployments_view.time_coverage_end) - min(abos_asfssots_all_deployments_view.time_coverage_start))) + (date_part('hours'::text, (max(abos_asfssots_all_deployments_view.time_coverage_end) - min(abos_asfssots_all_deployments_view.time_coverage_start))) / (24)::double precision)))::numeric) AS coverage_duration, (sum(abos_asfssots_all_deployments_view.coverage_duration))::integer AS data_coverage, CASE WHEN ((max(abos_asfssots_all_deployments_view.coverage_end) - min(abos_asfssots_all_deployments_view.coverage_start)) = 0) THEN 0 ELSE (((sum(abos_asfssots_all_deployments_view.coverage_duration) / ((max(abos_asfssots_all_deployments_view.coverage_end) - min(abos_asfssots_all_deployments_view.coverage_start)))::numeric) * (100)::numeric))::integer END AS percent_coverage, round(avg(abos_asfssots_all_deployments_view.mean_days_to_process_and_upload), 1) AS mean_days_to_process_and_upload, round(avg(abos_asfssots_all_deployments_view.mean_days_to_make_public), 1) AS mean_days_to_make_public, abos_asfssots_all_deployments_view.platform_code, abos_asfssots_all_deployments_view.sub_facility FROM abos_asfssots_all_deployments_view WHERE (abos_asfssots_all_deployments_view.headers IS NOT NULL) GROUP BY abos_asfssots_all_deployments_view.headers, abos_asfssots_all_deployments_view.data_category, abos_asfssots_all_deployments_view.data_type, abos_asfssots_all_deployments_view.file_type, abos_asfssots_all_deployments_view.platform_code, abos_asfssots_all_deployments_view.sub_facility ORDER BY abos_asfssots_all_deployments_view.file_type, abos_asfssots_all_deployments_view.headers, abos_asfssots_all_deployments_view.data_type, abos_asfssots_all_deployments_view.data_category;
+    SELECT 
+    v.file_type, 
+    v.headers, 
+    v.data_type, 
+    v.data_category, 
+    count(DISTINCT v.deployment_code) AS no_deployments, 
+    sum(v.no_fv1) AS no_fv1, 
+    sum(v.no_fv2) AS no_fv2, 
+    min(v.coverage_start) AS coverage_start, 
+    max(v.coverage_end) AS coverage_end, 
+    ceil(((date_part('day', (max(v.time_coverage_end) - min(v.time_coverage_start))) + (date_part('hours', (max(v.time_coverage_end) - min(v.time_coverage_start))) / (24)::double precision)))::numeric) AS coverage_duration, (sum(v.coverage_duration))::integer AS data_coverage, 
+    CASE WHEN max(v.coverage_end) - min(v.coverage_start) = 0 THEN 0 
+    ELSE (((sum(v.coverage_duration) / ((max(v.coverage_end) - min(v.coverage_start)))::numeric) * (100)::numeric))::integer END AS percent_coverage, 
+    round(avg(v.mean_days_to_process_and_upload), 1) AS mean_days_to_process_and_upload, 
+    round(avg(v.mean_days_to_make_public), 1) AS mean_days_to_make_public, 
+    v.platform_code, 
+    v.sub_facility 
+    FROM abos_asfssots_all_deployments_view v
+    WHERE v.headers IS NOT NULL 
+    GROUP BY v.headers, v.data_category, v.data_type, v.file_type, v.platform_code, v.sub_facility 
+    ORDER BY v.file_type, v.headers, v.data_type, v.data_category;
 
 grant all on table abos_asfssots_data_summary_view to public;
 
