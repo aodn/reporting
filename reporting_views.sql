cf043802d9567953a9263641b98b7f47c340a095
Xavier Hoenner <xavier.hoenner@utas.edu.au>
2014-04-23 11:17:31 +1000

    Update reporting views by copying in the content of acorn_views.sql

--- reporting_views.sql
+++ reporting_views.sql
@@ -190,11 +190,39 @@ CREATE or replace VIEW abos_asfssots_data_summary_view AS
 grant all on table abos_asfssots_data_summary_view to public;
 
 
+-------------------------------
+-- VIEW FOR ACORN; Still using the acorn_manual table from the report schema: needs to be updated to use the two acorn schemas - acorn_hourly_avg_nonqc & acorn_hourly_avg_qc
+-------------------------------
 -- has data
 -- acorn_manual -> report.acorn_manual
 
 CREATE or replace VIEW acorn_all_deployments_view AS
-    SELECT acorn_manual.code_type, CASE WHEN (acorn_manual.site_id = 1) THEN 'Capricorn Bunker Group'::text WHEN (acorn_manual.site_id = 2) THEN 'Rottnest Shelf'::text WHEN (acorn_manual.site_id = 3) THEN 'South Australia Gulf'::text WHEN (acorn_manual.site_id = 4) THEN 'Coffs Harbour'::text WHEN (acorn_manual.site_id = 5) THEN 'Turquoise Coast'::text ELSE 'Bonney Coast'::text END AS site, acorn_manual.code_full_name, date(acorn_manual.start_date_of_transmission) AS start, (acorn_manual.non_qc_data_availability_percent)::numeric AS non_qc_radial, (acorn_manual.non_qc_data_portal_percent)::numeric AS non_qc_grid, (acorn_manual.qc_data_availability_percent)::numeric AS qc_radial, (acorn_manual.qc_data_portal_percent)::numeric AS qc_grid, date(acorn_manual.last_qc_data_received) AS last_qc_date, date(acorn_manual.data_on_staging) AS data_on_staging, date(acorn_manual.data_on_opendap) AS data_on_opendap, date(acorn_manual.data_on_portal) AS data_on_portal, (date_part('day'::text, (acorn_manual.last_qc_data_received - acorn_manual.start_date_of_transmission)))::integer AS qc_coverage_duration, (date_part('day'::text, (acorn_manual.data_on_opendap - acorn_manual.start_date_of_transmission)))::integer AS days_to_process_and_upload, CASE WHEN (acorn_manual.data_on_portal IS NULL) THEN (date_part('day'::text, (acorn_manual.data_on_opendap - acorn_manual.start_date_of_transmission)))::integer ELSE (date_part('day'::text, (acorn_manual.data_on_portal - acorn_manual.data_on_opendap)))::integer END AS days_to_make_public, CASE WHEN (acorn_manual.mest_creation IS NULL) THEN 'No'::text ELSE 'Yes'::text END AS metadata FROM report.acorn_manual GROUP BY acorn_manual.code_type, acorn_manual.site_id, acorn_manual.code_full_name, acorn_manual.start_date_of_transmission, acorn_manual.non_qc_data_availability_percent, acorn_manual.non_qc_data_portal_percent, acorn_manual.qc_data_availability_percent, acorn_manual.qc_data_portal_percent, acorn_manual.last_qc_data_received, acorn_manual.data_on_staging, acorn_manual.data_on_opendap, acorn_manual.data_on_portal, acorn_manual.mest_creation ORDER BY CASE WHEN (acorn_manual.site_id = 1) THEN 'Capricorn Bunker Group'::text WHEN (acorn_manual.site_id = 2) THEN 'Rottnest Shelf'::text WHEN (acorn_manual.site_id = 3) THEN 'South Australia Gulf'::text WHEN (acorn_manual.site_id = 4) THEN 'Coffs Harbour'::text WHEN (acorn_manual.site_id = 5) THEN 'Turquoise Coast'::text ELSE 'Bonney Coast'::text END, acorn_manual.code_type, acorn_manual.code_full_name;
+    SELECT m.code_type, 
+    CASE WHEN m.site_id = 1 THEN 'Capricorn Bunker Group' 
+    WHEN m.site_id = 2 THEN 'Rottnest Shelf' 
+    WHEN m.site_id = 3 THEN 'South Australia Gulf' 
+    WHEN m.site_id = 4 THEN 'Coffs Harbour' 
+    WHEN m.site_id = 5 THEN 'Turquoise Coast' 
+    ELSE 'Bonney Coast' END AS site,
+    m.code_full_name,
+    date(m.start_date_of_transmission) AS start, 
+    (m.non_qc_data_availability_percent)::numeric AS non_qc_radial, 
+    (m.non_qc_data_portal_percent)::numeric AS non_qc_grid, 
+    (m.qc_data_availability_percent)::numeric AS qc_radial, 
+    (m.qc_data_portal_percent)::numeric AS qc_grid, 
+    date(m.last_qc_data_received) AS last_qc_date, 
+    date(m.data_on_staging) AS data_on_staging, 
+    date(m.data_on_opendap) AS data_on_opendap, 
+    date(m.data_on_portal) AS data_on_portal, 
+    (date_part('day'::text, (m.last_qc_data_received - m.start_date_of_transmission)))::integer AS qc_coverage_duration, 
+    (date_part('day'::text, (m.data_on_opendap - m.start_date_of_transmission)))::integer AS days_to_process_and_upload, 
+    CASE WHEN m.data_on_portal IS NULL THEN (date_part('day', (m.data_on_opendap - m.start_date_of_transmission)))::integer 
+    ELSE (date_part('day'::text, (m.data_on_portal - m.data_on_opendap)))::integer END AS days_to_make_public, 
+    CASE WHEN m.mest_creation IS NULL THEN 'No' 
+    ELSE 'Yes' END AS metadata 
+    FROM report.acorn_manual m
+    GROUP BY m.code_type, m.site_id, m.code_full_name, m.start_date_of_transmission, m.non_qc_data_availability_percent, m.non_qc_data_portal_percent, m.qc_data_availability_percent, m.qc_data_portal_percent, m.last_qc_data_received, m.data_on_staging, m.data_on_opendap, m.data_on_portal, m.mest_creation 
+    ORDER BY site, m.code_type, m.code_full_name;
 
 grant all on table acorn_all_deployments_view to public;
 
