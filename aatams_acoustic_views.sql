1a205a8ed1403983d9b0b79d703df1614c637764
Xavier Hoenner <xavier.hoenner@utas.edu.au>
2014-04-29 15:34:32 +1000

    Create all aatams_acoustic views

---
+++ aatams_acoustic_views.sql
@@ -0,0 +1,27 @@
+﻿SET SEARCH_PATH = report_test, public;
+
+CREATE OR REPLACE VIEW aatams_acoustictag_all_deployments_view AS
+    SELECT *
+    FROM dw_aatams_acoustic.aatams_acoustictag_all_deployments_view;
+
+CREATE OR REPLACE VIEW aatams_acoustictag_data_summary_project_view AS
+    SELECT *
+    FROM dw_aatams_acoustic.aatams_acoustictag_data_summary_project_view;
+
+CREATE OR REPLACE VIEW aatams_acoustictag_data_summary_species_view AS
+    SELECT *
+    FROM dw_aatams_acoustic.aatams_acoustictag_data_summary_species_view;
+
+CREATE OR REPLACE VIEW aatams_acoustictag_totals_project_view AS
+    SELECT *
+    FROM dw_aatams_acoustic.aatams_acoustictag_totals_project_view;
+
+CREATE OR REPLACE VIEW aatams_acoustictag_totals_species_view AS
+    SELECT *
+    FROM dw_aatams_acoustic.aatams_acoustictag_totals_species_view;
+
+grant all on table aatams_acoustictag_all_deployments_view to public;
+grant all on table aatams_acoustictag_data_summary_project_view to public;
+grant all on table aatams_acoustictag_data_summary_species_view to public;
+grant all on table aatams_acoustictag_totals_project_view to public;
+grant all on table aatams_acoustictag_totals_species_view to public;
