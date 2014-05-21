17585a93336e70c22ee7761c7c1a12a1820b8dd1
Xavier Hoenner <xavier.hoenner@utas.edu.au>
2014-05-21 12:20:35 +1000

    Updated facility_summary_view and modified corresponding Word document

--- report/reporting_views.sql
+++ report/reporting_views.sql
@@ -687,13 +687,20 @@ CREATE or replace VIEW auv_data_summary_view AS
 grant all on table auv_data_summary_view to public;
 
 
--- has data
--- facility_summary -> report.facility_summary
--- public.facility -> report.facility
--- facility_summary_item -> report.facility_summary_item 
+-------------------------------
+-- VIEW FOR Facility summary;
+-------------------------------
 
-CREATE or replace VIEW facility_summary_view AS
-    SELECT facility.acronym AS facility_acronym, COALESCE(((to_char(to_timestamp((date_part('month'::text, facility_summary.reporting_date))::text, 'MM'::text), 'TMMon'::text) || ' '::text) || date_part('year'::text, facility_summary.reporting_date))) AS reporting_month, facility_summary.summary AS updates, facility_summary_item.name AS issues, facility_summary.reporting_date FROM ((report.facility_summary FULL JOIN report.facility ON ((facility_summary.facility_name_id = facility.id))) LEFT JOIN report.facility_summary_item ON ((facility_summary.summary_item_id = facility_summary_item.row_id))) ORDER BY facility.acronym, facility_summary.reporting_date DESC, facility_summary_item.name;
+CREATE OR REPLACE VIEW facility_summary_view AS 
+  SELECT facility.acronym AS facility_acronym,
+  COALESCE(to_char(to_timestamp (date_part('month',facility_summary.reporting_date)::text, 'MM') ,'TMMon')||' '||date_part('year',facility_summary.reporting_date)) AS reporting_month,
+  facility_summary.summary AS updates, 
+  facility_summary_item.name AS issues,
+  facility_summary.reporting_date
+  FROM report.facility_summary
+  FULL JOIN report.facility ON facility_summary.facility_name_id = facility.id
+  LEFT JOIN report.facility_summary_item ON facility_summary.summary_item_id = facility_summary_item.row_id
+  ORDER BY facility_acronym, reporting_date DESC, issues;
 
 grant all on table facility_summary_view to public;
 
