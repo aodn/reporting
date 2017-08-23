682832eff7e83ae73876e7c35a0c6f7df71f5158
Xavier Hoenner <xavier.hoenner@utas.edu.au>
2017-08-24 09:47:36 +1000

    Update ReadMe and change schema name for aatams_acoustic reporting views

--- README.md
+++ README.md
@@ -1,22 +1,5 @@
-reporting
+Parameters mapping and Reporting
 =========
-
-The 'SQL_reporting_queries' folder contains all the SQL queries to generate the tables and views used to produce the IMOS data reports.
-
-The 'MS Word reporting templates' folder contains Word files detailing how the data reports should be produced using iReport.
-
-
-To address:
-* Argo and AATAMS acoustic reporting views contain too many records, Jac can't pdf the corresponding data reports.
-* AATAMS satellite tagging: 1/ Headers need to mention MEOP; 2/ Campaigns without species names; 3/ DM QC: Inconsistencies in species names; 4/ No DM QC in totals
-* Argo: 1/ No new data since end of June, no active floats; 2/ Query to generate views too slow; 3/ Problem with time range.
-* ABOS: Problem with time range.
-* ANMN: 
-    1/ Summary report too long; 
-      To make it shorter, we could try to combine all biogeochemical profiles colelcted at various different sites in South Australia.
-      Another improvement could be to create two types of reports: one for Active platforms and one for non-active ones.
-    2/ RT: Problem with time range + null site name.
-* ACORN: null station code for radials. Misspelling of Turquoise Coast site code
-* SOOP XBT NRT: Problem with time range.
-* FAIMMS: Problem with MYRWS site name. Should it be Myrmidon Reef instead? 
-* ANFOG:  Problem with total numbers of platforms and deployments in the "OceanGliders" summary report
+The 'parameters_mapping/SQL_metadata_summary_queries' folder contains contains all the SQL queries to generate the views for the CSV metadata headers.
+The 'report/SQL_reporting_queries' folder contains all the SQL queries to generate the tables and views used to produce the IMOS data reports.
+The 'report/MS Word reporting templates' folder contains Word files detailing how the data reports should be produced using iReport.
--- report/SQL_reporting_queries/aatams_acoustic.sql
+++ report/SQL_reporting_queries/aatams_acoustic.sql
@@ -536,7 +536,7 @@ DROP VIEW IF EXISTS aatams_acoustic_project_totals_view CASCADE;
 -- 	WHERE date_part('days',now() - latest_deployment_date) < 365.25;
 
 -- Create all views in reporting schema
-CREATE OR REPLACE VIEW aatams_acoustic_species_all_deployments_view AS SELECT * FROM dw_aatams_acoustic.aatams_acoustic_species_all_deployments_view;
+CREATE OR REPLACE VIEW aatams_acoustic_species_all_deployments_view AS SELECT * FROM aatams_acoustic_reporting.aatams_acoustic_species_all_deployments_view;
 CREATE OR REPLACE VIEW aatams_acoustic_species_data_summary_view AS 
 SELECT registered, 
 CASE WHEN common_name = '[a devilray]' THEN 'Devil Ray' WHEN
@@ -544,14 +544,14 @@ CASE WHEN common_name = '[a devilray]' THEN 'Devil Ray' WHEN
 	common_name = '[a whaler shark]' THEN 'Whaler Shark' WHEN
 	common_name = '[a wobbegong ]' THEN 'Wobbegong' ELSE common_name END AS common_name, 
 no_transmitters, no_releases, no_releases_with_location, no_embargo, no_protected, latest_embargo_date, total_no_detections, no_detections_public, earliest_detection, latest_detection, no_data_days
- FROM dw_aatams_acoustic.aatams_acoustic_species_data_summary_view
+ FROM aatams_acoustic_reporting.aatams_acoustic_species_data_summary_view
  ORDER BY registered, common_name;
-CREATE OR REPLACE VIEW aatams_acoustic_embargo_totals_view AS SELECT * FROM dw_aatams_acoustic.aatams_acoustic_embargo_totals_view;
-CREATE OR REPLACE VIEW aatams_acoustic_registered_totals_view AS SELECT * FROM dw_aatams_acoustic.aatams_acoustic_registered_totals_view;
-CREATE OR REPLACE VIEW aatams_acoustic_stats_view AS SELECT * FROM dw_aatams_acoustic.aatams_acoustic_stats_view;
-CREATE OR REPLACE VIEW aatams_acoustic_project_all_deployments_view AS SELECT * FROM dw_aatams_acoustic.aatams_acoustic_project_all_deployments_view ORDER BY project_name, installation_name, station_name;
-CREATE OR REPLACE VIEW aatams_acoustic_project_data_summary_view AS SELECT * FROM dw_aatams_acoustic.aatams_acoustic_project_data_summary_view;
-CREATE OR REPLACE VIEW aatams_acoustic_project_totals_view AS SELECT * FROM dw_aatams_acoustic.aatams_acoustic_project_totals_view;
+CREATE OR REPLACE VIEW aatams_acoustic_embargo_totals_view AS SELECT * FROM aatams_acoustic_reporting.aatams_acoustic_embargo_totals_view;
+CREATE OR REPLACE VIEW aatams_acoustic_registered_totals_view AS SELECT * FROM aatams_acoustic_reporting.aatams_acoustic_registered_totals_view;
+CREATE OR REPLACE VIEW aatams_acoustic_stats_view AS SELECT * FROM aatams_acoustic_reporting.aatams_acoustic_stats_view;
+CREATE OR REPLACE VIEW aatams_acoustic_project_all_deployments_view AS SELECT * FROM aatams_acoustic_reporting.aatams_acoustic_project_all_deployments_view ORDER BY project_name, installation_name, station_name;
+CREATE OR REPLACE VIEW aatams_acoustic_project_data_summary_view AS SELECT * FROM aatams_acoustic_reporting.aatams_acoustic_project_data_summary_view;
+CREATE OR REPLACE VIEW aatams_acoustic_project_totals_view AS SELECT * FROM aatams_acoustic_reporting.aatams_acoustic_project_totals_view;
 
 grant all on table aatams_acoustic_species_all_deployments_view to public;
 grant all on table aatams_acoustic_species_data_summary_view to public;
