6e44d79e6dbc78b90eff38733e14b0406a2d9016
Hugo Oliveira <ocehugo@gmail.com>
2020-12-04 11:57:21 +1100

    fix(trigger): update src email and error handling
    Handle a missing error case and update the email
    to be sent by gmail address.

--- report/trigger_report_and_email.sh
+++ report/trigger_report_and_email.sh
@@ -2,7 +2,7 @@
 
 
 email_trigger() {
-    ./reporting_email_sender.sh "$cdate" ../report_logs/"$cdate" ../report_statuses/"$cdate" ../figures/eMII_data_report/AATAMS_EmbargoPlots/EmbargoPlot_"$zdate".jpeg hugo.oliveira@utas.edu.au "hugo.oliveira@utas.edu.au,jacqui.hope@utas.edu.au"
+    ./reporting_email_sender.sh "$cdate" ../report_logs/"$cdate" ../report_statuses/"$cdate" ../figures/eMII_data_report/AATAMS_EmbargoPlots/EmbargoPlot_"$zdate".jpeg aodnreporting@gmail.com "hugo.oliveira@utas.edu.au" #"jacqui.hope@utas.edu.au"
 }
 
 # Drive the report script
@@ -16,4 +16,6 @@ for k in $(seq 1 $ntry);do
     [ -n "$failreport" ] && echo "Error: Report script failed" && echo "Bad" > ../report_statuses/"$cdate"
     [ -n "$failcheck" ] && echo "Error: Incomplete Tables" && echo "Bad" > ../report_statuses/"$cdate"
     { [ -z "$failreport" ] || [ -z "$failcheck" ]; } && echo "SUCCESS: All reporting tables are alive and updated." && echo "Good" > ../report_statuses/"$cdate" && echo "Good" > ../checker_statuses/"$cdate" && email_trigger && break
+#    { [ -z "$failreport" ] || [ -z "$failcheck" ]; } && echo "SUCCESS: All reporting tables are alive and updated." && echo "Good" > ../report_statuses/"$cdate" && echo "Good" > ../checker_statuses/"$cdate" && break
+
 done
